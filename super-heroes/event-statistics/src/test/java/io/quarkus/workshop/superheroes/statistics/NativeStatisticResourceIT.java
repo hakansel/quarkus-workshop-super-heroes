fd31f95c7685fc43256a0cefb962b5405d518768
hakansel <hakansel05@gmail.com>
2019-10-23 16:52:07 +0300

    event-statistics init
    event subscriber

---
+++ super-heroes/event-statistics/src/test/java/io/quarkus/workshop/superheroes/statistics/NativeStatisticResourceIT.java
@@ -0,0 +1,9 @@
+package io.quarkus.workshop.superheroes.statistics;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeStatisticResourceIT extends StatisticResourceTest {
+
+    // Execute the same tests but in native mode.
+}
